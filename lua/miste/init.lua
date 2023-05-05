2935c402190e97e2f6ac867ace4186af91c1242e
Vincent Edwards <mister.man489@gmail.com>
2023-05-05 14:09:05 -0700

    highlight on yank

--- lua/miste/init.lua
+++ lua/miste/init.lua
@@ -5,3 +5,10 @@ require('onedark').setup {
   style = 'darker'
 }
 require('onedark').load()
+
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=150})
+augroup END
+]]
