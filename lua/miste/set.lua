cf7325cd9b499b8f6e44d26d637614df1c99a0c3
Vincent Edwards <mister.man489@gmail.com>
2023-05-05 18:54:07 -0700

    spellcheck all files

--- lua/miste/set.lua
+++ lua/miste/set.lua
@@ -25,17 +25,4 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.opt.spelllang = "en_us"
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "tex",
-	callback = function()
-		vim.opt_local.spell = true
-	end
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt_local.spell = true
-	end
-})
+vim.opt.spell = true
