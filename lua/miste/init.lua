94fb008a8994befee87f5983b7482831b3c9b55d
Vincent Edwards <mister.man489@gmail.com>
2023-07-20 18:11:07 -0700

    comments added to init.lua

--- lua/miste/init.lua
+++ lua/miste/init.lua
@@ -1,11 +1,14 @@
+-- Use the other files in this directory
 require("miste.set")
 require("miste.remap")
 
+-- Use the onedark darker theme
 require('onedark').setup {
   style = 'darker'
 }
 require('onedark').load()
 
+-- Flash a highlight on yank
 vim.cmd[[
 augroup highlight_yank
 autocmd!
