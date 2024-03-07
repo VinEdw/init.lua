fa8c3949debe7d32b9375fbe7d627bbf97e35701
Vincent Edwards <mister.man489@gmail.com>
2024-03-06 19:14:55 -0800

    Switched from packer.nvim to lazy.nvim

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Neovim Lua Configuration
 
-This is my Neovim configuration.
-It will always be a work in progress, but it has been fairly stable for a while.
-The plugin manager is [packer.nvim](https://github.com/wbthomason/packer.nvim).
-Feel free to use it as inspiration for your own config!
+This is my [Neovim](https://neovim.io/) configuration.
+It will probably always be a work in progress.
+The plugin manager is [lazy.nvim](https://github.com/folke/lazy.nvim).
+I'm probably doing things in a suboptimal way.
+Still, feel free to use it as inspiration for your own config!
--- lua/miste/init.lua
+++ lua/miste/init.lua
@@ -1,13 +1,69 @@
 -- Use the other files in this directory
 require("miste.set")
 require("miste.remap")
-require("miste.packer")
 
--- Use the onedark darker theme
-require('onedark').setup {
-  style = 'darker'
-}
-require('onedark').load()
+-- Ensure lazy.nvim is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Install plugins
+require("lazy").setup({
+  -- Onedark editor theme
+  {
+    "navarasu/onedark.nvim",
+    opts = { style = "darker" },
+    config = function(_, opts)
+      require("onedark").setup(opts)
+      require("onedark").load()
+    end,
+  },
+
+  -- Telescope fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+
+  -- Treesitter syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  -- LSP Plugins
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip",
+  -- Vimscript plugins
+  "tpope/vim-surround",
+  "tpope/vim-repeat",
+  "tpope/vim-commentary",
+  "tpope/vim-fugitive",
+  "justinmk/vim-sneak",
+  "wellle/targets.vim",
+  "michaeljsmith/vim-indent-object",
+  "inkarkat/vim-ReplaceWithRegister",
+  -- Typst plugin
+  {
+    "kaarmu/typst.vim",
+    ft = "typst",
+    lazy = false,
+  },
+})
 
 -- Flash a highlight on yank
 vim.cmd[[
