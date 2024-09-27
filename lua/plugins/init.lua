06fc2d3f820e4840415d8184f4304241d04360ab
Vincent Edwards <mister.man489@gmail.com>
2024-09-26 21:42:13 -0700

    Vim-sneak config put in its own file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,8 @@
 return {
-  -- Vimscript plugins
   "tpope/vim-surround",
   "tpope/vim-repeat",
   "tpope/vim-commentary",
   "tpope/vim-fugitive",
-  "justinmk/vim-sneak",
   "wellle/targets.vim",
   "michaeljsmith/vim-indent-object",
   "inkarkat/vim-ReplaceWithRegister",
---
+++ lua/plugins/sneak.lua
@@ -0,0 +1,11 @@
+return {
+  "justinmk/vim-sneak",
+  lazy = false,
+  config = function()
+    -- Use vim-sneak's versions of f, F, t, & T
+    vim.keymap.set("n", "f", "<Plug>Sneak_f")
+    vim.keymap.set("n", "F", "<Plug>Sneak_F")
+    vim.keymap.set("n", "t", "<Plug>Sneak_t")
+    vim.keymap.set("n", "T", "<Plug>Sneak_T")
+  end
+}
