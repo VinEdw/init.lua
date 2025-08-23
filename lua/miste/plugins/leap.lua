f6c43445c2fadf865704104df31b0fddcb1cb870
Vincent Edwards <mister.man489@gmail.com>
2025-08-23 14:12:51 -0700

    Replace sneak with leap plugin

---
+++ lua/miste/plugins/leap.lua
@@ -0,0 +1,12 @@
+return {
+  "ggandor/leap.nvim",
+  config = function()
+    local leap = require('leap')
+    leap.opts.case_sensitive = true
+    vim.keymap.set({'n', 'x'}, 's',  '<Plug>(leap-forward)')
+    vim.keymap.set('o',        'z',  '<Plug>(leap-forward)')
+    vim.keymap.set('n',        'S',  '<Plug>(leap-backward)')
+    vim.keymap.set('o',        'Z',  '<Plug>(leap-backward)')
+    vim.keymap.set('n',        'gs', '<Plug>(leap-from-window)')
+  end
+}
