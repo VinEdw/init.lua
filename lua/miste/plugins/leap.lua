4dcc6d3705bd78a1dfdc7781aa9e610a95bb5c86
Vincent Edwards <mister.man489@gmail.com>
2025-08-30 21:56:21 -0700

    Update z to leap-till when used with commands

--- lua/miste/plugins/leap.lua
+++ lua/miste/plugins/leap.lua
@@ -4,9 +4,9 @@ return {
     local leap = require('leap')
     leap.opts.case_sensitive = true
     vim.keymap.set({'n', 'x'}, 's',  '<Plug>(leap-forward)')
-    vim.keymap.set('o',        'z',  '<Plug>(leap-forward)')
+    vim.keymap.set('o',        'z',  '<Plug>(leap-forward-till)')
     vim.keymap.set('n',        'S',  '<Plug>(leap-backward)')
-    vim.keymap.set('o',        'Z',  '<Plug>(leap-backward)')
+    vim.keymap.set('o',        'Z',  '<Plug>(leap-backward-till)')
     vim.keymap.set('n',        'gs', '<Plug>(leap-from-window)')
   end
 }
