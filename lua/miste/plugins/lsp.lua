7f23e8ca3465b0715d02b60e44ca71c493b6e778
Vincent Edwards <mister.man489@gmail.com>
2025-11-23 23:40:18 -0800

    Remove Mason, simplify LSP setup, and fix julials

--- lua/miste/plugins/lsp.lua
+++ lua/miste/plugins/lsp.lua
@@ -4,53 +4,84 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
   },
   config = function()
-    -- note: diagnostics are not exclusive to lsp servers
-    -- so these can be global keybindings
-    vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end)
-    vim.keymap.set('n', '[d', function() vim.diagnostic.jump({count=-1, float=true}) end)
-    vim.keymap.set('n', ']d', function() vim.diagnostic.jump({count=1, float=true}) end)
 
+    -- Set buffer-local keybindings where there is an active language server
     vim.api.nvim_create_autocmd('LspAttach', {
       desc = 'LSP actions',
       callback = function(event)
         local opts = {buffer = event.buf}
-
-        -- these will be buffer-local keybindings
-        -- because they only work if you have an active language server
-
-        vim.keymap.set('n', 'gh', function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-        vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
-        vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
-        vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', function() vim.lsp.buf.format({async = true}) end, opts)
-        vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, opts)
       end
     })
 
+    -- Connect to nvim-cmp package
     local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
     local default_setup = function(server)
       require('lspconfig')[server].setup({
         capabilities = lsp_capabilities,
       })
     end
 
-    require('mason-lspconfig').setup({
-      ensure_installed = {},
-      handlers = {
-        default_setup,
-      },
-    })
+    -- Enable the desired language servers
+    -- Included is a short note on how to install each
 
-    vim.lsp.config('julials', {})
+    -- dnf install clang clang-tools-extra
+    vim.lsp.enable('clangd')
+
+    -- julia --project=@nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer"); Pkg.add("SymbolServer"); Pkg.add("StaticLint")'
+    -- julia --project=@nvim-lspconfig -e 'using Pkg; Pkg.update()'
+    vim.lsp.config('julials', {
+      on_new_config = function(new_config,new_root_dir)
+      server_path = "~/.julia/environments/nvim-lspconfig"
+      cmd = {
+        "julia",
+        "--project="..server_path,
+        "--startup-file=no",
+        "--history-file=no",
+        "-e", [[
+          using Pkg
+          Pkg.instantiate()
+          using LanguageServer
+          depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
+          project_path = let
+              dirname(something(
+                  ## 1. Finds an explicitly set project (JULIA_PROJECT)
+                  Base.load_path_expand((
+                      p = get(ENV, "JULIA_PROJECT", nothing);
+                      p === nothing ? nothing : isempty(p) ? nothing : p
+                  )),
+                  ## 2. Look for a Project.toml file in the current working directory,
+                  ##    or parent directories, with $HOME as an upper boundary
+                  Base.current_project(),
+                  ## 3. First entry in the load path
+                  get(Base.load_path(), 1, nothing),
+                  ## 4. Fallback to default global environment,
+                  ##    this is more or less unreachable
+                  Base.load_path_expand("@v#.#"),
+              ))
+          end
+          @info "Running language server" VERSION pwd() project_path depot_path
+          server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path)
+          server.runlinter = true
+          run(server)
+        ]]
+      };
+      new_config.cmd = cmd
+    end
+    })
     vim.lsp.enable('julials')
+
+    -- https://github.com/Myriad-Dreamin/tinymist/releases
+    vim.lsp.enable('tinymist')
+
+    -- npm install -g typescript-language-server typescript
+    vim.lsp.enable('ts_ls')
+
+    -- npm install -g pyright
+    vim.lsp.enable('pyright')
 
   end,
 }
