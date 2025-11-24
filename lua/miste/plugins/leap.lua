9ba1d8cca6ed62092b132c3ddb4e1bd282398cc2
Vincent Edwards <mister.man489@gmail.com>
2025-11-24 00:52:27 -0800

    Tweak leap keybinds

--- lua/miste/plugins/leap.lua
+++ lua/miste/plugins/leap.lua
@@ -3,10 +3,9 @@ return {
   config = function()
     local leap = require('leap')
     leap.opts.case_sensitive = true
-    vim.keymap.set({'n', 'x'}, 's',  '<Plug>(leap-forward)')
-    vim.keymap.set('o',        'z',  '<Plug>(leap-forward-till)')
+    vim.keymap.set('n',        's',  '<Plug>(leap-forward)')
+    vim.keymap.set({'o', 'x'}, 'z',  '<Plug>(leap-forward-till)')
     vim.keymap.set('n',        'S',  '<Plug>(leap-backward)')
-    vim.keymap.set('o',        'Z',  '<Plug>(leap-backward-till)')
-    vim.keymap.set('n',        'gs', '<Plug>(leap-from-window)')
+    vim.keymap.set({'o', 'x'}, 'Z',  '<Plug>(leap-backward-till)')
   end
 }
