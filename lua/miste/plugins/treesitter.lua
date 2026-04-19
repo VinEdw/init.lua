107bf16d9b80be0bb05c921a5a7a78c56eacb1d6
Vincent Edwards <mister.man489@gmail.com>
2026-04-19 15:51:57 -0700

    Switch from nvim-treesitter to tree-sitter-manager
    Old package was archived.

--- lua/miste/plugins/treesitter.lua
+++ lua/miste/plugins/treesitter.lua
@@ -1,9 +1,8 @@
 return {
-  -- Treesitter syntax highlighting
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    require("nvim-treesitter.configs").setup({
+  "romus204/tree-sitter-manager.nvim",
+  dependencies = {}, -- tree-sitter CLI must be installed system-wide
+  config = function()
+    require("tree-sitter-manager").setup({
       ensure_installed = {
         "javascript",
         "html",
@@ -22,24 +21,6 @@ return {
         "awk",
         "ruby",
       },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = false,
-
-      highlight = {
-        enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
     })
-    -- require("nvim-treesitter.install").compilers = { "gcc", "clang" }
   end
 }
