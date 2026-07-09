6255341b6f540a46db389c962a1ab278c241f907
Vincent Edwards <mister.man489@gmail.com>
2026-07-08 20:43:05 -0700

    Remove ensure_installed list
    Avoids issues on devices where treesitter is not installed

--- lua/miste/plugins/treesitter.lua
+++ lua/miste/plugins/treesitter.lua
@@ -3,24 +3,6 @@ return {
   dependencies = {}, -- tree-sitter CLI must be installed system-wide
   config = function()
     require("tree-sitter-manager").setup({
-      ensure_installed = {
-        "javascript",
-        "html",
-        "css",
-        "json",
-        "python",
-        "lua",
-        "vim",
-        "vimdoc",
-        "gitcommit",
-        "diff",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "cpp",
-        "awk",
-        "ruby",
-      },
     })
   end
 }
